4120ad0c5f83dc3ec6556e8011280ee0e1699932
Leo Lima <leonardo.lima.silva@ccc.ufcg.edu.br>
2020-07-29 18:50:12 -0300

    copy lib and adding my functions

---
+++ AutoComplete/AutoComplete.js
@@ -0,0 +1,144 @@
+import React, { Component } from 'react'
+import { TouchableHighlight, Text, View } from 'react-native'
+import {TextInput} from 'react-native-paper';
+import PropTypes from 'prop-types';
+import stringScore from 'string_score'
+import Styles from './Styles'
+
+class AutoComplete extends Component {
+  componentDidMount () {
+    this.suggestions = this.filterSugestions(
+      this.props.suggestions, this.props.value
+    )
+  }
+
+  componentWillUpdate (nextProps, nextState) {
+    this.suggestions = this.filterSugestions(
+      nextProps.suggestions, nextProps.value
+    )
+  }
+
+  getSuggestionText = (suggestion) => {
+    if (this.props.suggestionObjectTextProperty) {
+      return suggestion[this.props.suggestionObjectTextProperty]
+    }
+
+    return suggestion
+  }
+
+  isSimilar = (value, suggestionText) => {
+
+    const newValue = value.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    const newSuggestionText = suggestionText.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    const suggestionScore = stringScore(
+      newSuggestionText, newValue, this.props.comparationFuzziness
+    )
+
+    return suggestionScore >= this.props.minimumSimilarityScore
+  }
+
+  shouldFilterSuggestions = (newSuggestions, value) => {
+    return newSuggestions && newSuggestions.length &&
+      value && !this.selectedSuggestion
+  }
+
+  filterSugestions = (newSuggestions, value) => {
+    if (!this.shouldFilterSuggestions(newSuggestions, value)) {
+      return {}
+    }
+
+    return newSuggestions.reduce((suggestions, suggestion) => {
+      const suggestionText = this.getSuggestionText(suggestion)
+
+      if (!suggestionText || !this.isSimilar(value, suggestionText)) {
+        return suggestions
+      }
+
+      suggestions[suggestionText] = suggestion
+      return suggestions
+    }, {})
+  }
+
+  onChangeText = (value) => {
+    this.selectedSuggestion = false
+
+    if (this.props.onChangeText) {
+      this.props.onChangeText(value)
+    }
+  }
+
+  suggestionClick = (suggestion) => () => {
+    this.selectedSuggestion = true
+    this.suggestions = {}
+    this.props.onSelect(suggestion)
+  }
+
+  renderSuggestions = () => {
+    const suggestionTexts = Object.keys(this.suggestions || {})
+
+    if (!suggestionTexts.length) {
+      return null
+    }
+
+    return (
+      <View
+        style={this.props.suggestionsWrapperStyle || Styles.suggestionsWrapper}
+      >
+        {
+          suggestionTexts.map((text, index) => (
+            <TouchableHighlight
+              key={index}
+              suggestionText={text}
+              activeOpacity={0.6}
+              style={this.props.suggestionStyle || Styles.suggestion}
+              onPress={this.suggestionClick(this.suggestions[text])}
+              underlayColor='white'
+            >
+              <Text
+                style={this.props.suggestionTextStyle || Styles.suggestionText}
+              >
+                {text}
+              </Text>
+            </TouchableHighlight>
+          ))
+        }
+      </View>
+    )
+  }
+
+  render () {
+    return (
+      <View style={this.props.style || Styles.wrapper}>
+        <TextInput
+          {...this.props}
+          onChangeText={this.onChangeText}
+          style={this.props.inputStyle || Styles.input}
+        />
+
+        {this.renderSuggestions()}
+      </View>
+    )
+  }
+}
+
+AutoComplete.propTypes = {
+  suggestions: PropTypes.array,
+  value: PropTypes.string,
+  minimumSimilarityScore: PropTypes.number,
+  comparationFuzziness: PropTypes.number,
+  suggestionObjectTextProperty: PropTypes.string,
+  onChangeText: PropTypes.func,
+  onSelect: PropTypes.func.isRequired,
+  suggestionsWrapperStyle: PropTypes.any,
+  suggestionStyle: PropTypes.any,
+  suggestionTextStyle: PropTypes.any,
+  style: PropTypes.any,
+  inputStyle: PropTypes.any
+}
+
+AutoComplete.defaultProps = {
+  minimumSimilarityScore: 0.6,
+  comparationFuzziness: 0.5
+}
+
+export default AutoComplete
---
+++ README.md
@@ -0,0 +1,7 @@
+# react-native-autocomplete-select
+
+npm install --save react-native-autocomplete-select
+
+reference `https://github.com/Vizir/react-native-autocomplete-select`
+
+modifications to accept lowercase letters and without accents in the input
